92e6e1483758c1117e14e5c0715f3cd3c267d7f5
GeorgePearseKubrick <56070024+GeorgePearseKubrick@users.noreply.github.com>
2021-01-04 22:44:13 +0000

    Update beholdAI with new data

--- BeholdAI.py
+++ BeholdAI.py
@@ -1,10 +1,13 @@
+
 import json
+import logging
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
 from datetime import datetime as dt
-from sklearn.metrics import fbeta_score
+from sklearn.metrics import fbeta_score, confusion_matrix
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
@@ -60,6 +63,8 @@ def hospital_records_cleaning(hospital_records_df):
     hospital_records_df.drop_duplicates(subset=['Accession Number'], keep=False, inplace=True) # has no effect
     logging.append(len(hospital_records_df))
 
+    hospital_records_df.rename(columns={'Normal':'Normal_hard_class'}, inplace=True)
+    hospital_records_df.set_index('Accession Number', inplace=True)
     return hospital_records_df, logging
 
 
@@ -82,6 +87,8 @@ def model_outputs_cleaning(model_outputs_df):
     # two of the same sample with different labels -> impossible to know which is the 'correct' label.
     model_outputs_df.drop_duplicates(subset=['accession_number'], keep=False, inplace=True)
     logging.append(len(model_outputs_df))
+
+    model_outputs_df.set_index('accession_number', inplace=True)
     return model_outputs_df, logging, model_outputs_dirty
 
 
@@ -104,6 +111,8 @@ def radiologist_labels_cleaning(radiologist_labels_df):
     # two of the same sample with different labels -> impossible to know which is the 'correct' label.
     radiologist_labels_df.drop_duplicates(subset=['dicom_elements.value'], keep=False, inplace=True)
     logging.append(len(radiologist_labels_df))
+
+    radiologist_labels_df.set_index('dicom_elements.value', inplace=True)
     return radiologist_labels_df, logging
 
 
@@ -118,24 +127,26 @@ rows with identical values accross columns) and the removal of rows with differe
 This is based on the assumption that there is no way to confidently determine what data should be kept and which removed. 
 """)
 
+# left_column, right_column = st.beta_columns(2)
+# pressed = left_column.button('Filter NAs')
+# if pressed:
+#     pas
 
 data_cleaning = st.beta_expander("Data Checks & Cleaning")
 data_cleaning.write(f"""
 * Length of model-outputs before any cleaning **{model_outputs_logging[0]}**. \n
 * Length of model-outputs after removing _perfect_ duplicates **{model_outputs_logging[1]}**. \n
-* Length of model-outputs after removing corrupt data (duplicate accession numbers) **{model_outputs_logging[2]}**. _(review this)_\n 
-
+* Length of model-outputs after removing corrupt data (duplicate accession numbers) **{model_outputs_logging[2]}**. \n 
 * Length of radiologist-labels before any cleaning **{radiologist_labels_logging[0]}**. \n
 * Length of radiologist-labels after removing _perfect_ duplicates **{radiologist_labels_logging[1]}**. \n
 * Length of radiologist-labels after removing corrupt data (duplicate accession numbers) **{radiologist_labels_logging[2]}**. \n
-
 * Length of radiologist-labels before any cleaning **{hospital_records_logging[0]}**. \n
 * Length of radiologist-labels after removing _perfect_ duplicates **{hospital_records_logging[1]}**. \n
 * Length of radiologist-labels after removing corrupt data (duplicate accession numbers) **{hospital_records_logging[2]}**. \n
 """)
 
 
-def select_institute(df, institution):
+def select_institute(df,  institute):
     if institute != 'All':
         df = df[df['institution'] == institute]
     return df
@@ -165,7 +176,7 @@ mean_time_hours, mean_time_minutes, mean_time_seconds = split_dt(mean_time)
 median_time_hours, median_time_minutes, median_time_seconds = split_dt(median_time)
 
 st.write(f"""
-* Total number of exams processed is **{hospital_records_df['Accession Number'].nunique()}**.
+* Total number of exams processed is **{len(set(hospital_records_df.index))}**.
 * Mean average time taken to process image is **{mean_time_hours} hours \
                                                        {mean_time_minutes} minutes \
                                                        {mean_time_seconds} seconds**.
@@ -173,15 +184,27 @@ st.write(f"""
                                                        {median_time_minutes} minutes \
                                                        {median_time_seconds} seconds**.
 """)
+
+def retrieve_results_of_threshold(combined_dataset, abnormal_threshold):
+    """
+    Calculate the metrics for every threshold to improve the load performance while tweaking. Greater upfront cost
+    but much more interactive
+    """
+    logging.info(f"Columns in combined_dataset at this point are {combined_dataset.columns}.")
+    combined_dataset['is_normal_pred'] = combined_dataset['Normal'].apply(lambda x: 1 if x > (1-abnormal_threshold) else 0) 
+    count_normal = combined_dataset['is_normal_pred'].sum()
+    count_abnormal = len(combined_dataset) - count_normal
+    return combined_dataset, count_normal, count_abnormal
+
                                                     
+def calc_fbeta(combined_dataset, beta, ab_threshold):
+    combined_dataset, _, _ = retrieve_results_of_threshold(combined_dataset, ab_threshold)
+    return fbeta_score(combined_dataset['is_normal_radiologist'], combined_dataset['is_normal_pred'], beta=beta)
 
 st.markdown("""
 ## Classification Metrics
-
-_**NB** - These are not hospital specific._ \n
 In the context of radiology, false positives (where a positive in this case is defined by being classified as normal /
 cancer free) are much more costly than false negatives. 
-
 With this definition of positive, precision should be prioritised. The F-beta score is an effective way to balance 
 the importance of precision against recall.
 """)
@@ -190,61 +213,19 @@ st.write(r"""
 $$
 F \beta Score = (1 + \beta^2) * \frac{(1 + \beta^2) * TP}{(1 + \beta^2) * TP + \beta^2 * FN + FP}
 $$
-
 Where the user attributes $\beta$ times as much importance to recall as precision.
-
 * **_Low precision_** = undetected cancer patient's treatment gets delayed.
 * **_Low recall_** = unnecessary further imaging and medical treatment.
 """)
 
+def concat_datasets(radiologist_labels_df, model_outputs_df, hospital_records_df):
 
+    return model_outputs_df.merge(radiologist_labels_df, how='inner', left_index=True, right_index=True)\
+                           .merge(hospital_records_df, how='inner', left_index=True, right_index=True)
 
-def join_datasets(radiologist_labels_df, model_outputs_df):
-    radiologist_model = pd.merge(radiologist_labels_df, 
-                            model_outputs_df, 
-                            how='inner',
-                            left_on='dicom_elements.value', 
-                            right_on='accession_number') 
-    return radiologist_model
+combined_dataset = concat_datasets(radiologist_labels_df, model_outputs_df, hospital_records_df)
 
-radiologist_model = join_datasets(radiologist_labels_df, model_outputs_df)
-
-st.write(f"""
-## Data Quality
-
-* Number of rows lost from radiologist labels in the join = **{len(radiologist_labels_df) - len(radiologist_model)}**
-* Number of rows lost from hospital records in the join = **{len(model_outputs_df) - len(radiologist_model)}**
-* Total number of rows remaining after inner join is **{len(radiologist_model)}**
-
-""")
-
-
-def retrieve_results_of_threshold(abnormal_threshold, radiologist_model):
-    """
-    Calculate the metrics for every threshold to improve the load performance while tweaking. Greater upfront cost
-    but much more interactive
-    """
-    radiologist_model['is_normal'] = radiologist_model['Normal'].apply(lambda x: 1 if x < abnormal_threshold else 0)
-    count_normal = radiologist_model['is_normal'].sum()
-    count_abnormal = len(radiologist_model) - count_normal
-    return radiologist_model, count_normal, count_abnormal
-
-
-def calc_fbeta(radiologist_model, beta, ab_threshold):
-    radiologist_model['is_normal'] = radiologist_model['Normal'].apply(lambda x: 1 if x < ab_threshold else 0)
-    # fbeta_score(y_true, y_pred)
-    return fbeta_score(radiologist_model['is_normal_radiologist'], radiologist_model['is_normal'], beta=beta)
-    
-
-abnormal_threshold = st.slider('Abnormal Threshold', 0.00, 1.00, value=0.5)  
-radiologist_model, count_normal, count_abnormal = retrieve_results_of_threshold(abnormal_threshold, radiologist_model)
-
-# number classified as abnormal should go down as you increase the threshold (literally what increasing a threshold means)
-st.write(f"Number classified as abnormal at this threshold = **{count_abnormal}**")
-st.write(f"Number classified as normal at this threshold = **{count_normal}**")
-
-
-def return_fbeta_score_plot(radiologist_model, beta):
+def return_fbeta_score_plot(combined_dataset, beta):
     """
     Plot a line graph of the f1 score at different thresholds, just put a line at the selected value.
     """
@@ -253,54 +234,51 @@ def return_fbeta_score_plot(radiologist_model, beta):
     for ab_threshold in range(0,100):
         ab_threshold = ab_threshold/100
         ab_thresholds.append(ab_threshold)
-        f_beta_score = calc_fbeta(radiologist_model, beta, ab_threshold)
+        f_beta_score = calc_fbeta(combined_dataset, beta, ab_threshold)
         f_beta_scores.append(f_beta_score)
 
     fbeta_scores_df = pd.DataFrame({'F Beta-Score': f_beta_scores, 'Abnormal Threshold': ab_thresholds})
     # Having this marker probably significantly slows down rendering because it will have to reload for every change.
-    return px.line(fbeta_scores_df, x='Abnormal Threshold', y='F Beta-Score').update_layout(
-                                                                            shapes=[dict(
-                                                                                        type= 'line',
-                                                                                        yref= 'paper', y0= 0, y1= 1,
-                                                                                        xref= 'x', x0= abnormal_threshold
-                                                                                                 , x1= abnormal_threshold,
-                                                                                                 line=dict(
-                                                                                                    color="Red",
-                                                                                                    width=4,
-                                                                                                    dash="dashdot",
-                                                                                                )
-                                                                                    )])\
-                                                              .update_traces({'line':{'color':'black'}})\
-                                                              .update_layout({
-                                                                        'plot_bgcolor': 'rgb(224, 208, 208)'
-                                                                    })
-
+    return px.line(fbeta_scores_df, x='Abnormal Threshold', y='F Beta-Score')\
+        .update_traces({"line":{"color":"black"}})\
+        .update_layout(
+        shapes=[dict(
+                    type= 'line',
+                    yref= 'paper', y0= 0, y1= 1,
+                    xref= 'x', x0= abnormal_threshold
+                                , x1= abnormal_threshold,
+                                line=dict(
+                                color="Red",
+                                width=4,
+                                dash="dashdot",
+                            )
+                )])\
+        .update_layout({
+            'plot_bgcolor': 'rgb(224, 208, 208)'
+        })
 
 
 beta = st.slider('Beta', 0.01, 2.0, value=0.5)  
 st.write(f"""
 Set beta to 1.0 to retrieve the F1 Score. For the reasons already discussed the default is set to 0.5 
 meaning that precision is 2_x_ more important than recall. In the cancer classification context this is 
-probably the maximum reasonable value.
-
+probably the maximum reasonable value. \n
 The selected value of beta (**{beta}**) means that you consider recall **{beta}**_x_ more important than 
 precision and precision **{str(1/beta)[:4]}**_x_ more important than recall.
+
+The f-beta score from the model's hard classification for **{institute}** at a beta of **{beta}** is 
+**{str(fbeta_score(combined_dataset['is_normal_radiologist'], combined_dataset['Normal_hard_class'], beta=beta))[:4]}**.
 """)
 
+abnormal_threshold = st.slider('Abnormal Threshold', 0.00, 1.00, value=0.5)  
+combined_dataset, count_normal, count_abnormal = retrieve_results_of_threshold(combined_dataset, abnormal_threshold)
 
-st.write(return_fbeta_score_plot(radiologist_model, beta))
-f_beta = calc_fbeta(radiologist_model, beta, abnormal_threshold)
+# number classified as abnormal should go down as you increase the threshold (literally what increasing a threshold means)
+st.write(f"Number classified as abnormal at this threshold = **{count_abnormal}**")
+st.write(f"Number classified as normal at this threshold = **{count_normal}**")
+
+
+st.write(return_fbeta_score_plot(combined_dataset, beta))
+f_beta = calc_fbeta(combined_dataset, beta, abnormal_threshold)
 st.write(f"F beta-score at that beta (**{str(beta)[:4]}**) and that threshold (**{abnormal_threshold}**) is **{str(f_beta)[:4]}**.")
 
-
-expander = st.beta_expander("Brief Aside")
-expander.write("""
-Since my original application I've discovered the Msc - Computer Graphics, Vision and Imaging at UCL. 
-While acceptance is no sure thing 3 of the modules may as well be described as training for Behold AI.
-
-* Computational Modelling for Biomedical Imaging (15 credits)
-* Information Processing in Medical Imaging (15 credits)
-* Inverse Problems in Imaging (15 credits) 
-
-https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/computer-graphics-vision-imaging-msc
-""")
